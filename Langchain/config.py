86754dd04b60d6d73f0e17b14b4338d0f729b8f1
Arpit Mishra <arpit@Arpits-MacBook-Pro.local>
2023-06-27 16:11:30 +0530

    Rule update

---
+++ Langchain/config.py
@@ -0,0 +1,4 @@
+# key =   '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'
+key = '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'
+# hugging_face = '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'
+serp_api = 'eefa4a24d04365d257506d6434c09d7d6a2f79674b9e4ad58d69c01d0b43ec45'
